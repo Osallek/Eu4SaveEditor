c52d98256d9e5337a658937c8e2725e8bdf07347
GaetanRAYNAUD <gaetanraynaud@outlook.com>
2022-12-26 21:38:13 +0100

    Loading the game before the save

--- src/main/java/fr/osallek/eu4saveeditor/common/ReadSaveStep.java
+++ src/main/java/fr/osallek/eu4saveeditor/common/ReadSaveStep.java
@@ -7,15 +7,16 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public enum ReadSaveStep {
-    META_DATA("players_countries", 0),
-    PROVINCES("provinces", 1),
-    COUNTRIES("countries", 2),
-    WARS("active_advisors", 3),
-    GAME("idea_dates", 4);
+    GAME(null, 0),
+    META_DATA("players_countries", 50),
+    PROVINCES("provinces", 55),
+    COUNTRIES("countries", 65),
+    WARS("active_advisors", 95),
+    FINISHED("idea_dates", 100);
 
     public final String itemName;
 
-    public final int step;
+    public final int progress;
 
     private static final Map<String, ReadSaveStep> BY_ITEM_NAME;
 
@@ -23,20 +24,30 @@ public enum ReadSaveStep {
 
     static {
         NB_STEPS = ReadSaveStep.values().length;
-        BY_ITEM_NAME = Arrays.stream(ReadSaveStep.values()).collect(Collectors.toMap(ReadSaveStep::getItemName, Function.identity()));
+        BY_ITEM_NAME = Arrays.stream(ReadSaveStep.values())
+                             .filter(step -> step.itemName != null)
+                             .collect(Collectors.toMap(ReadSaveStep::getItemName, Function.identity()));
     }
 
-    ReadSaveStep(String itemName, int step) {
+    ReadSaveStep(String itemName, int progress) {
         this.itemName = itemName;
-        this.step = step;
+        this.progress = progress;
+    }
+
+    public ReadSaveStep next() {
+        if (ordinal() >= ReadSaveStep.values().length - 1) {
+            return ReadSaveStep.values()[0];
+        } else {
+            return ReadSaveStep.values()[ordinal() + 1];
+        }
     }
 
     public String getItemName() {
         return itemName;
     }
 
-    public int getStep() {
-        return step;
+    public int getProgress() {
+        return progress;
     }
 
     public static Optional<ReadSaveStep> getStep(String itemName) {
--- src/main/java/fr/osallek/eu4saveeditor/common/ReadSaveTask.java
+++ src/main/java/fr/osallek/eu4saveeditor/common/ReadSaveTask.java
@@ -2,12 +2,15 @@ package fr.osallek.eu4saveeditor.common;
 
 import fr.osallek.clausewitzparser.model.ClausewitzItem;
 import fr.osallek.eu4parser.Eu4Parser;
+import fr.osallek.eu4parser.model.game.Game;
 import fr.osallek.eu4parser.model.save.Save;
+import javafx.concurrent.Task;
+import org.springframework.context.MessageSource;
+
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
-import javafx.concurrent.Task;
-import org.springframework.context.MessageSource;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ReadSaveTask extends Task<Save> {
 
@@ -21,20 +24,33 @@ public class ReadSaveTask extends Task<Save> {
         this.gameDirectory = gameDirectory;
         this.saveFile = saveFile;
         this.messageSource = messageSource;
-        getProgress(ReadSaveStep.values()[0].itemName); //Init
+
+        //Init
+        updateProgress(0, 100);
+        updateTitle(this.messageSource.getMessage("ose.progress." + ReadSaveStep.values()[0].name(), null, Constants.LOCALE));
     }
 
     @Override
     protected Save call() throws Exception {
-        return Eu4Parser.loadSave(this.gameDirectory.getValue().getAbsoluteFile().toPath(),
-                                  this.saveFile,
-                                  Eu4Parser.loadSettings(this.gameDirectory.getValue().getAbsoluteFile().toPath()), new HashMap<>(),
+        AtomicInteger count = new AtomicInteger(0);
+        Game game = Eu4Parser.parseGame(this.gameDirectory.getValue().getAbsoluteFile().toPath(), Eu4Parser.getMods(this.saveFile, new HashMap<>()),
+                                        Eu4Parser.loadSettings(this.gameDirectory.getValue().getAbsoluteFile().toPath()),
+                                        () -> {
+                                            count.incrementAndGet();
+                                            int progress = ReadSaveStep.GAME.progress;
+                                            progress += (ReadSaveStep.GAME.next().progress - ReadSaveStep.GAME.progress) * count.get()
+                                                        / (Game.NB_PARTS + 1);
+
+                                            updateProgress(progress, 100);
+                                        });
+
+        return Eu4Parser.loadSave(this.saveFile, game, new HashMap<>(),
                                   Map.of(item -> ClausewitzItem.DEFAULT_NAME.equals(item.getParent().getName()), this::getProgress));
     }
 
     private void getProgress(String itemName) {
         ReadSaveStep.getStep(itemName).ifPresent(step -> {
-            updateProgress(step.step, ReadSaveStep.NB_STEPS);
+            updateProgress(step.progress, 100);
             updateTitle(this.messageSource.getMessage("ose.progress." + step.name(), null, Constants.LOCALE));
         });
     }
